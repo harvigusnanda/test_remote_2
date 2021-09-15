39f2ae5ab28da189bd4ec6b687887604f87afdab
harvigusnanda <gusnandaharvi@gmail.com>
2021-09-15 11:07:57 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint praesentium laborum minima sed deleniti est quae vitae quasi! Officia et hic quis beatae corporis laudantium consequatur illo voluptatem sequi veritatis!</p>
 </body>
 </html>
